30e93f3ab38bd1efa52243cfa71c894b76691491
trdybo <94643628+trdybo@users.noreply.github.com>
2021-12-14 14:20:55 -0800

    Add files via upload

--- resources.js
+++ resources.js
@@ -13,7 +13,7 @@ function write(element, content = "", append = false) {
 
 function requirePassword (returnTo = "/") {
 	document.getElementsByClassName("page")[0].style.visibility = "hidden";
-	if (localStorage.getItem('login') == null || (Date.now() - localStorage.getItem('login')) >= 10 * (1000 * 60)) {
+	if (localStorage.getItem('login') == null || (Date.now() - parseInt(localStorage.getItem('login'), 10) >= 10 * (1000 * 60)) {
 		window.location.href = "/Login/";
 		localStorage.setItem('hashCheck', -2872ae81);
 		localStorage.setItem('returnTo', returnTo);
