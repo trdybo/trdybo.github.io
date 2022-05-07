f88db81d4502764578df75eaddc56cc72da6e282
trdybo <94643628+trdybo@users.noreply.github.com>
2022-05-07 14:21:15 -0700

    Update resources.js

--- resources.js
+++ resources.js
@@ -15,7 +15,7 @@ function requirePassword (returnTo = "/") {
 	document.getElementsByClassName("page")[0].style.visibility = "hidden";
 	if (localStorage.getItem('login') == null || (Date.now() - parseInt(localStorage.getItem('login'), 10) >= 10 * (1000 * 60))) {
 		window.location.href = "/Login/";
-		localStorage.setItem('hashCheck', "-2872ae81");
+		localStorage.setItem('hashCheck', "-a804001");
 		localStorage.setItem('returnTo', returnTo);
 	} else {
 		document.getElementsByClassName("page")[0].style.visibility = "visible";
