d45ec9635861310ae1974ac7520b887bee182ba4
trdybo <94643628+trdybo@users.noreply.github.com>
2022-05-07 14:15:27 -0700

    Unbad

--- resources.js
+++ resources.js
@@ -13,9 +13,9 @@ function write(element, content = "", append = false) {
 
 function requirePassword (returnTo = "/") {
 	document.getElementsByClassName("page")[0].style.visibility = "hidden";
-	if (localStorage.getItem('login') == null || (Date.now() - parseInt(localStorage.getItem('login'), 10) >= 10 * (1000 * 60)) {
+	if (localStorage.getItem('login') == null || (Date.now() - parseInt(localStorage.getItem('login'), 10) >= 10 * (1000 * 60))) {
 		window.location.href = "/Login/";
-		localStorage.setItem('hashCheck', -2872ae81);
+		localStorage.setItem('hashCheck', "-2872ae81");
 		localStorage.setItem('returnTo', returnTo);
 	} else {
 		document.getElementsByClassName("page")[0].style.visibility = "visible";
